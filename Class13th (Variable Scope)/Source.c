395185fa6277b98c31a20f729b36a2c7e1dc3c5f
sth6197 <sth6197@gmail.com>
2024-04-01 19:45:47 +0900

    (2024.04.01) 동적 할당

---
+++ Class13th (Variable Scope)/Source.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+
+int itemCode;
+
+int level = 1;
+
+void Stage()
+{
+	level += 1;
+}
+
+void Score()
+{
+	// 정적 변수는 단 한 번만 초기화가 이루어지는
+	// 변수입니다.
+	static int score = 0;
+
+	score++;
+
+	printf("score 변수의 값 : %d\n", score);
+
+}
+int main()
+{
+#pragma region 지역 변수
+	// 함수 내부에서 선언된 변수로 { } 내에서만 접근
+	// 할 수 있으며, { }를 벗어나게 되면 메모리에서 사라지는 특징을 가지고 있습니다.
+
+	// int data = 100;
+	// {
+	// 		int data = 200;
+	// 
+	// 		// 가장 가까운 위치에 있는 data 변수에 접근합니다.
+	// 		printf("data 변수의 값 : %d\n", data);
+	// }
+	// printf("data 변수의 값 : %d\n", data);
+
+#pragma endregion
+
+#pragma region 전역 변수
+	// 함수 외부에 선언된 변수로, 어디에서든지 접근이 가능하며
+	// 프로그램이 실행될 때 생성되고, 프로그램이 종료될 때 메모리에서
+	// 해제되는 특징을 가지고 있는 변수입니다.
+
+	// Stage();
+	// 
+	// printf("level 변수의 값 : %d\n", level);
+	// printf("itemCode 변수의 값 : %d\n", itemCode);
+
+
+#pragma endregion
+
+#pragma region 정적 변수
+	// static 이라는 키워드를 사용하고 지역 변수의 특성을 가지면서, 
+	// 전역 변수의 특징을 가지고 있는 변수입니다.
+
+	// Score();
+	// Score();
+	// Score();
+
+
+
+#pragma endregion
+
+#pragma region 소인수 분해
+	//
+	// int a = 0;
+	// 
+	// printf("입력한 숫자 : ");
+	// scanf_s("%d", &a);
+	// printf("결과값 : ");
+	// 
+	// for (int i = 2; i <= a; i++)
+	// {
+	// 		if (a % i == 0)
+	// 		{
+	// 			a = a / i;
+	// 			printf("%d ", i);
+	// 			i--;
+	// 		}
+	// }
+#pragma endregion
+
+	return 0;
+}
--- Program/Source.c
+++ Program/Source.c
@@ -1,84 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int itemCode;
 
-int level = 1;
-
-void Stage()
-{
-	level += 1;
-}
-
-void Score()
-{
-	// 정적 변수는 단 한 번만 초기화가 이루어지는
-	// 변수입니다.
-	static int score = 0;
-
-	score++;
-
-	printf("score 변수의 값 : %d\n", score);
-
-}
 int main()
 {
-#pragma region 지역 변수
-	// 함수 내부에서 선언된 변수로 { } 내에서만 접근
-	// 할 수 있으며, { }를 벗어나게 되면 메모리에서 사라지는 특징을 가지고 있습니다.
+#pragma region 동적 할당
+	// 프로그램을 실행 중에 필요한 만큼 메모리를 할당하는 작업입니다.
 
-	// int data = 100;
-	// {
-	// 		int data = 200;
+	// int* ptr = (int*)malloc(sizeof(int));
 	// 
-	// 		// 가장 가까운 위치에 있는 data 변수에 접근합니다.
-	// 		printf("data 변수의 값 : %d\n", data);
-	// }
-	// printf("data 변수의 값 : %d\n", data);
-	
-#pragma endregion
-
-#pragma region 전역 변수
-	// 함수 외부에 선언된 변수로, 어디에서든지 접근이 가능하며
-	// 프로그램이 실행될 때 생성되고, 프로그램이 종료될 때 메모리에서
-	// 해제되는 특징을 가지고 있는 변수입니다.
-
-	// Stage();
+	// // 동적 할당은 실행 시간에 가변적으로 메모리의 크기를 변경시킬 수 있으며,
+	// // 동적으로 메모리의 크기를 할당할 때 바이트 단위로 반환합니다.
 	// 
-	// printf("level 변수의 값 : %d\n", level);
-	// printf("itemCode 변수의 값 : %d\n", itemCode);
+	// printf("ptr이 가리키는 값 : %d\n", *ptr);
+	// 
+	// *ptr = 100;
+	// 
+	// printf("ptr이 가리키는 값 : %d\n", *ptr);
+	// 
+	// // 메모리를 동적 할당할 때 주소를 범용 포인터로 반환하기 때문에
+	// // 자료형을 변환한 다음 메모리에 할당해야 합니다.
+	// 
+	// free(ptr); 
+	// free(ptr);
 
 
+	// 동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로 사용이 끝나면
+	// 직접 해제해주어야 합니다. (free();)
 #pragma endregion
 
-#pragma region 정적 변수
-	// static 이라는 키워드를 사용하고 지역 변수의 특성을 가지면서, 
-	// 전역 변수의 특징을 가지고 있는 변수입니다.
+#pragma region 댕글링 포인터
+	// 이미 해제된 메모리 영역을 가리키는 포인터입니다.
 
-	// Score();
-	// Score();
-	// Score();
+	// int* ptr1 = malloc(4);
+	// 
+	// *ptr1 = 100;
+	// 
+	// printf("ptr1이 가리키는 값 : %d\n", *ptr1);
+	// 
+	// free(ptr1);
+	// 
+	// ptr1 = NULL;
+	// 
+	// // 해제된 메모리 공간에 접근해서 값을 저장하였습니다.
+	// *ptr1 = 999;
+	// 
+	// printf("ptr1이 가리키는 값 : %d\n", *ptr1);
 
-
-
+	// 해제된 포인터에 NULL; 값을 넣는다.
 #pragma endregion
 
-#pragma region 소인수 분해
+#pragma region 동적 배열
 	
-	int a = 0;
-
-	printf("입력한 숫자 : ");
-	scanf_s("%d", &a);
-	printf("결과값 : ");
-
-	for (int i = 2; i <= a; i++)
-	{
-		if (a % i == 0)
-		{
-			a = a / i;
-			printf("%d ", i);
-			i--;
-		}
-	}
+	//	int dynamicArraySize = 5;
+	//	
+	//	int* arrPtr = (int*)malloc(sizeof(int) * dynamicArraySize);
+	//	
+	//	int size = (sizeof(int)* dynamicArraySize)/sizeof(int);
+	//	
+	//	for (int i = 0; i < size; i++)
+	//	{
+	//		arrPtr[i] = i + 1;
+	//		printf("arrPtr[%] : %d\n", i, arrPtr[i]);
+	//		
+	//	}
+	//	
+	//	free(arrPtr);
+	//	
+	//	dynamicArraySize = 10;
+	//	
+	//	arrPtr = (int*)malloc(sizeof(int) * dynamicArraySize);
+	//	
+	//	size = (sizeof(int) * dynamicArraySize) / sizeof(int);
+	//	
+	//	printf("\n");
+	//	
+	//	for (int i = 0; i < size; i++)
+	//	{
+	//		arrPtr[i] = i + 1;
+	//		printf("arrPtr[%] : %d\n", i, arrPtr[i]);
+	//	
+	//	}
+	//	
+	//	free(arrPtr);
 #pragma endregion
 
 	return 0;
